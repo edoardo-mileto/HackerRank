905f0903c6e7b1ce6fae802ebdcdb9cd5fda72a4
Edoardo Mileto <edomileto@gmail.com>
2020-10-29 14:22:38 +0100

    add C - priting tokens

---
+++ C/03. Arrays and Strings/03. Printing Tokens.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+/*
+   https://www.hackerrank.com/challenges/printing-tokens-
+ */
+
+int main() {
+
+	char *s;
+	s = malloc(1024 * sizeof(char));
+	scanf("%[^\n]", s);
+	s = realloc(s, strlen(s) + 1);
+
+	for(int i = 0; i < strlen(s); i++)
+	{
+		if(s[i] == ' ')
+			printf("\n");
+		else
+			printf("%c",s[i]);
+	}
+
+	return 0;
+}
