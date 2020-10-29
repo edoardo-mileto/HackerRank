1e4d90868c620e9ab058326568ba65dee43b8b57
Edoardo Mileto <edomileto@gmail.com>
2020-10-29 13:54:07 +0100

    update - add problem url for 1D arrays in c

--- C/03. Arrays and Strings/01. 1D Arrays.c
+++ C/03. Arrays and Strings/01. 1D Arrays.c
@@ -3,14 +3,18 @@
 #include <math.h>
 #include <stdlib.h>
 
+/*
+   https://www.hackerrank.com/challenges/1d-arrays-in-c
+ */
+
 int main(){
-  int n_size, i, sum = 0;
-  scanf("%d", &n_size);
-  for (i = 0; i < n_size; i++){
-    int tmp;
-    scanf("%d", &tmp);
-    sum += tmp;
-  }
-  printf("%d\n", sum);
+	int n_size, i, sum = 0;
+	scanf("%d", &n_size);
+	for (i = 0; i < n_size; i++) {
+		int tmp;
+		scanf("%d", &tmp);
+		sum += tmp;
+	}
+	printf("%d\n", sum);
 	return 0;
 }
